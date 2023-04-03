fb7f6dbad215b884bd39fa6053be08ad5876aff3
Fitz Gerald <gfitzsicat@gmail,com>
2023-04-03 15:30:57 -0400

    testing

---
+++ solution.js
@@ -0,0 +1,17 @@
+/*
+The squirrels in Palo Alto spend most of the day playing. In particular, they play if the temperature is between 60 and 90 (inclusive). Unless it is summer, then the upper limit is 100 instead of 90. Given an int temperature and a boolean is_summer, return true if the squirrels play and false otherwise.
+*/
+
+function squirrel_play(temp, is_summer) {
+    if (summer = true && temperature >=60 && temperature <= 100) {
+    return true;
+    } else if (summer = false && temperature <=50) {
+        return false;
+    } else if (summer = true && temperature >=60 && temperature <= 100 ) {
+        return true;
+    } else if (summer = false && temperature >= 60 && temperature <= 90) {
+        return true;
+    }
+
+}
+export default squirrel_play
